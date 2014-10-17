8b27f93b6aaa38d76f2abee4b5ee6392593dd8b3
Peter Román <peterrom@gmail.com>
2014-10-18 00:56:22 +0200

    Merge branch 'master' of https://github.com/peterrom/gomokai

--- src/eddsonGomokuClient.cpp
+++ src/eddsonGomokuClient.cpp
@@ -1,8 +1,38 @@
 #include "eddsonGomokuClient.h"
+const Coordinate null_coord(-1, -1);
 
+void eddsonGomokuClient::getBoardPoints(Board* board, BOARD_POINTS* boardPoints) {
+	for (auto i = 0; i < 19*19; i++)
+		boardPoints->push_back(board->point(Coordinate(i % 19, i / 19)));
+}
 
-eddsonGomokuClient::eddsonGomokuClient()
+Coordinate eddsonGomokuClient::getEnemysLastMove(BOARD_POINTS* currentBoard, BOARD_POINTS* lastBoard) {
+	Coordinate coord = null_coord;
+	for (auto i = 0; i < 19*19; i++)
+	{
+		if ((*currentBoard)[i] != EMPTY && (*currentBoard)[i] != this->m_color && (*currentBoard)[i] != (*lastBoard)[i])
+		{
+			coord = Coordinate(i % 19, i / 19);
+			break;
+		}
+	}
+	return coord;
+}
+
+Coordinate eddsonGomokuClient::predictNextMove(Coordinate* currentMove, Coordinate* lastMove) {
+	int dx = lastMove->x - currentMove->x;
+	int dy = currentMove->y - lastMove->y;
+	if (dx > 0) dx = 1;
+	if (dx < 0) dx = -1;
+	if (dy > 0) dy = 1;
+	if (dy < 0) dy = -1;
+
+	return Coordinate(dx, dy);
+}
+
+eddsonGomokuClient::eddsonGomokuClient(PointType color) : GomokuClient(color), lastMove(0,0)
 {
+	lastBoard = new BOARD_POINTS();
 }
 
 
@@ -12,5 +42,13 @@ eddsonGomokuClient::~eddsonGomokuClient()
 
 Coordinate eddsonGomokuClient::make_a_move(Board& board) {
 	Coordinate coord(0,0);
+	BOARD_POINTS *currentBoard = new BOARD_POINTS();
+	getBoardPoints(&board, currentBoard);
+	Coordinate currentMove = this->getEnemysLastMove(currentBoard, lastBoard);
+
+	coord = currentMove;
+
+	lastMove = currentMove;
+	lastBoard = currentBoard;
 	return coord;
 }
--- src/eddsonGomokuClient.h
+++ src/eddsonGomokuClient.h
@@ -1,12 +1,23 @@
 #pragma once
 #include "gomoku_client.hpp"
+
+#define BOARD_POINTS std::vector<PointType>
+
 class eddsonGomokuClient :
 	public GomokuClient
 {
 public:
-	eddsonGomokuClient();
+	eddsonGomokuClient(PointType);
 	~eddsonGomokuClient();
 
 	Coordinate make_a_move(Board&);
+
+private:
+	Coordinate lastMove;
+	std::vector<PointType>* lastBoard;
+
+	void getBoardPoints(Board*, BOARD_POINTS*);
+	Coordinate getEnemysLastMove(BOARD_POINTS*, BOARD_POINTS*);
+	Coordinate predictNextMove(Coordinate*, Coordinate*);
 };
 
